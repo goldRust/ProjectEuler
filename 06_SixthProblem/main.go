a1fd20a6713751bf3c7762372f722a88840c1edc
Dustin Stucky <dustin.j.stucky@gmail.com>
2017-09-04 15:43:45 -0500

    First Commit

--- 06_SixthProblem/main.go
+++ 06_SixthProblem/main.go
@@ -1,1 +1,19 @@
-package _6_SixthProblem
+package main
+
+import "fmt"
+
+func main(){
+	var sum int
+	var sqsum int
+	var sumsq int
+	for i:=1;i<=100;i++{
+		sumsq += i*i
+	}
+
+	for i:=1;i<=100;i++{
+		sum +=i
+	}
+	sqsum = sum*sum
+	fmt.Println(sqsum - sumsq)
+}
+
--- 07_SeventhProblem/main.go
+++ 07_SeventhProblem/main.go
@@ -1,1 +1,28 @@
-package _7_SeventhProblem
+package main
+
+import "fmt"
+
+func main(){
+	counter:=0
+	i:=1
+	for {
+		i++
+		if isPrime(i){
+			counter++
+		fmt.Println(counter," - ",i)
+		}
+		if counter == 10001 {
+			fmt.Print(i)
+			break
+		}
+	}
+}
+func isPrime(num int)bool{
+	for i:=2;i<num;i++{
+		if num%i==0{
+			return false
+		}
+
+	}
+	return true
+}
