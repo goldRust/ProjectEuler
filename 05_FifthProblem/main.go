658ac48660d55e18c3acdc1fbb83e428bcb5b4d9
Dustin Stucky <dustin.j.stucky@gmail.com>
2017-09-04 01:57:49 -0500

    Explaination added.

--- 05_FifthProblem/main.go
+++ 05_FifthProblem/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main(){
 	var num int
 	i:=1
+	//run loop until divBy returns true
 	for {
 
 		if divBy(i){num=i
@@ -12,14 +13,18 @@ func main(){
 
 		i++
 	}
+	//Print the smallest multiple
 	fmt.Println(num)
 
 }
 	func divBy(num int)bool{
+		//cycle through 1 - 20
 		for i:=1;i<=20;i++{
+			//eliminate any numbers that are not divisible by i
 			if num%i!=0{
 				return false
 			}
 		}
+		// return true if the int made it through
 		return true
 	}
